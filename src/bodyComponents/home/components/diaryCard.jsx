6b8860af1d6280493697e08caed0848b630f39af
Hui Sun <j.sun71839@gmail.com>
2019-04-12 18:42:00 -0400

    add save favorite articles to database

--- src/bodyComponents/home/components/diaryCard.jsx
+++ src/bodyComponents/home/components/diaryCard.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
-import { Card, Row, Col, Avatar, Icon } from "antd";
-import { MDBCard, MDBRow, MDBMask, MDBView, MDBIcon, MDBBtn, MDBCardBody, MDBCardTitle, MDBContainer, MDBCol } from "mdbreact";
+import { Row, Col, Avatar, Icon } from "antd";
+import { MDBCard, MDBRow, MDBMask, MDBView, MDBIcon, MDBBtn, MDBCardBody, MDBCardTitle } from "mdbreact";
 import { Button } from "react-bootstrap";
 import { actionCreators } from "../store";
+import AuthorRecommend from "./authorRecommend";
 import "../home.css";
 import { Link } from "react-router-dom";
 
 class DiaryCard extends Component {
   getDiaryCardList() {
-    const { diaryCardList, page } = this.props;
+    const { diaryCardList, page, handleSaveArticle, loginUser } = this.props;
 
     const newDiaryCardList = diaryCardList.toJS();
 
@@ -21,17 +22,17 @@ class DiaryCard extends Component {
         if (newDiaryCardList[i]) {
           cardPerPageList.push(
             <Col className="gutter-row" span={6} key={i}>
-              <MDBCard className="diaryCard">
+              <MDBCard>
                 <MDBView hover zoom className="cardView">
-                  <img alt="diary pic" src={newDiaryCardList[i]["cover_imgURL"]} style={{ width: "320px", height: "220px" }} />
+                  <img alt="diary pic" src={newDiaryCardList[i]["cover_imgURL"]} style={{ width: "100%", height: "200px" }} />
                   <MDBMask overlay="stylish-light">
-                    <MDBBtn color="danger" rounded size="xl">
+                    <MDBBtn color="danger" rounded size="xl" onClick={() => handleSaveArticle(newDiaryCardList[i]["post_id"], loginUser._id)}>
                       <MDBIcon fas="true" icon="thumbtack" className="left" />
                       <b> Save</b>
                     </MDBBtn>
                     <span className="spanIcon">
-                      <MDBIcon icon="heart" size="lg" color="white" className="ml-2" />
-                      {}
+                      <MDBIcon icon="heart" color="white" className="ml-2" />
+                      {newDiaryCardList[i]["likes"]}
                     </span>
                   </MDBMask>
                 </MDBView>
@@ -63,18 +64,27 @@ class DiaryCard extends Component {
   render() {
     const { page, totalPage, handlePageChange } = this.props;
     return (
-      <div>
-        <Row className="mdbRow">{this.getDiaryCardList()}</Row>
-        <Row style={{ marginTop: 28 }}>
-          <Button
-            className="btn btn btn-outline-info"
-            style={{ display: "block", margin: "0 auto" }}
-            onClick={() => handlePageChange(page, totalPage)}
-          >
-            More Diaries
-          </Button>
+      <Fragment>
+        <Row>
+          <Col span={19}>
+            <div className="diaryCards">
+              <Row>
+                {this.getDiaryCardList()}
+              </Row>
+              <Row className="moreDiaryButtonRow">
+                <Button className="btn btn btn-outline-info" onClick={() => handlePageChange(page, totalPage)}>
+                  More Diaries
+                </Button>
+              </Row>        
+            </div>
+          </Col>
+          <Col span={5}>
+            <div className="authorRecommend">
+              <AuthorRecommend/>
+            </div>
+          </Col>
         </Row>
-      </div>
+      </Fragment>
     );
   }
 
@@ -89,7 +99,8 @@ const mapStateToProps = state => {
   return {
     diaryCardList: state.getIn(["home", "diaryCardList"]),
     page: state.getIn(["home", "page"]),
-    totalPage: state.getIn(["home", "totalPage"])
+    totalPage: state.getIn(["home", "totalPage"]),
+    loginUser: state.getIn(["login", "loginUser"])    
   };
 };
 
@@ -105,7 +116,10 @@ const mapDispatchToProps = dispatch => {
       } else {
         dispatch(actionCreators.pageChangeAction(0));
       }
-    }
+    },
+    handleSaveArticle(post_id, user_id) {
+      dispatch(actionCreators.handleSaveArticleAction(post_id, user_id));
+    }  
   };
 };
 
--- src/bodyComponents/message/components/chat.jsx
+++ src/bodyComponents/message/components/chat.jsx
@@ -1,29 +1,123 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import ReceiveMsg from './receiveMsg';
-import SendMsg from "./sendMsg";
-import { Layout } from 'antd';
-
-const {
-    Header, Footer, Sider, Content,
-} = Layout;
-
-
+import {ChatHeader, ChatWrap, ReceiveMsg, SendMsg, ChatContentWrap, SenderWrap} from "../style";
+import {actionCreators} from "../store";
+import {Map,List} from "immutable";
 
 
 class Chat extends Component {
-    render() {
-        return (
-            <div className="wrap chatDialogue" >
-                <Header>chat with me</Header>
-                <ReceiveMsg />
-                <SendMsg />
-            </div>
-        )
-    }
+	state={
+		content:""
+	}
+
+	handleSend = () => {
+		const {loginUser,sendMsg} = this.props;
+		const from = loginUser._id;
+		const to = this.props.match.params.userid;
+		const content = this.state.content.trim();
+
+		//Send msg
+		if(content){
+			sendMsg({from,to,content});
+		}
+		this.setState({content:''});
+	}
+
+	componentDidMount(){
+		window.scroll(0,document.body.scrollHeight);
+	}
+
+	componentUpdateMount(){
+		window.scroll(0,document.body.scrollHeight);
+	}
+
+
+
+	render() {
+		const {loginUser,msgList} = this.props;
+		
+		//calculate the current chat_id
+		const myId = loginUser._id;
+		
+		//if no data, nothing is shown 
+		// if(!Map(msgList.users).count()){
+		// 	return null;
+		// }
+
+		const targetId= this.props.match.params.userid;
+
+		const tartgetUser = Map(msgList.users).get(targetId);
+
+		const currentchatId = [myId,targetId].sort().join("_");
+
+		const chatMsgs = List(msgList.chatMsgs);
+
+		//***filter chatMsgs***
+		chatMsgs.filter(msg=>msg.chat_id===currentchatId);
+
+		//get the target user avatar and make sure the avatar is existed
+		const targetAvatar =Map(tartgetUser).get("avatar")?(
+			<img src={Map(tartgetUser).get("avatar")} className="card-img rounded-circle msgAvatar" alt="avartar"/>
+		):null;
+
+		return (
+			<ChatWrap>
+					<ChatHeader>Chat with {Map(tartgetUser).get("username")}</ChatHeader>
+				<ChatContentWrap>
+				
+	
+					{chatMsgs.map(msg=>{
+						if(myId === msg.to){ //msg to me
+						    return (
+								<ReceiveMsg key={msg._id}>
+									{targetAvatar}
+									<span>{msg.content}</span>
+								</ReceiveMsg>
+							)
+						}else{ //msg from me
+							return(
+								<SendMsg key={msg._id}>
+									<span>{msg.content}</span>
+								</SendMsg>
+							)
+						}
+					})}
+				</ChatContentWrap>
+				<SenderWrap className="input-group mb-3">
+					<textarea 
+						type="text" 
+						className="col-10 form-control" 
+						placeholder="Message" 
+						rows={1}
+						value={this.state.content}
+						onChange={e => this.setState({content:e.target.value})}
+					/>
+					<div className="input-group-append col-2">
+						
+						<button className="btn btn-outline-default col-12" onClick={this.handleSend.bind(this)}><span>🙈</span>Send</button>
+					</div>
+				</SenderWrap>
+			</ChatWrap>
+		)
+	}
+}
+
+const mapStateToProps = (state) => {
+	return {
+		loginUser: state.getIn(["login", "loginUser"]),
+		msgList:state.getIn(["message","msgList"])
+	}
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+		sendMsg({from,to,content}){
+			dispatch(actionCreators.handleSendMsgAction({from,to,content}));
+		}
+	}
 }
 
 export default connect(
-    state => ({}),
-    {}
+	mapStateToProps,
+	mapDispatchToProps
 )(Chat);
